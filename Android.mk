7a2a45a95e516b21d3d48f3b4fcd08c359cb3fc3
Angel <mino_forever@yahoo.com>
2016-01-18 21:24:02 +0200

    link spn

--- Android.mk
+++ Android.mk
@@ -141,4 +141,10 @@ $(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
     ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
 	    $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 
+# Create symlink spn-conf.xml which points to selective-spn-conf.xml
+# since Huawei's modem does not pull in network operator names
+$(shell mkdir -p $(TARGET_OUT)/etc; \
+    ln -sf /system/etc/selective-spn-conf.xml \
+	   $(TARGET_OUT)/etc/spn-conf.xml)
+	   
 endif
