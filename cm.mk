e5e41bc2487bc890500334d72a784c83051ee5f6
Angel <mino_forever@yahoo.com>
2016-01-18 07:08:11 +0200

    first commit
    ril fix

---
+++ cm.mk
@@ -0,0 +1,44 @@
+#
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Specify phone tech before including full_phone
+#$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit some common CM stuff
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/msm8610/full_msm8610.mk)
+
+# Correct boot animation size for the screen
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
+
+# Device name
+PRODUCT_DEVICE := msm8610
+PRODUCT_NAME := cm_msm8610
+PRODUCT_BRAND := huawei
+PRODUCT_MODEL := msm8610
+PRODUCT_MANUFACTURER := huawei
+PRODUCT_RELEASE_NAME := msm8610
+
+# CM packages
+PRODUCT_PACKAGES += \
+    Torch
